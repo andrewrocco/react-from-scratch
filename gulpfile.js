f3adb1bf8fc625850d54ed96c5c65c943d80b8e3
Andy Rossi <aroccorossi@gmail.com>
2016-02-16 20:38:31 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+// gulpfile.js
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var source      = require('vinyl-source-stream');
+var babelify    = require('babelify');
+var watchify    = require('watchify');
+var browserify  = require('browserify');
+var browserSync = require('browser-sync').create();
+
+watchify.args.debug = true;
+var bundler = watchify(browserify('./src/script.js', watchify.args));
+
+bundler.transform(babelify.configure({
+  presets: ["es2015", "react"]
+}));
+
+bundler.on('update', bundle);
+
+function bundle() {
+
+  gutil.log('Compiling JS...');
+
+  return bundler.bundle()
+    .on('error', function (err) {
+      gutil.log(err.message);
+      browserSync.notify("Browserify Error!");
+      this.emit("end");
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream({once: true}));
+}
+
+gulp.task('bundle', function () {
+  return bundle();
+});
+
+gulp.task('default', ['bundle'], function () {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch('index.html', function() {
+    browserSync.reload();
+  })
+});
