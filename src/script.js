588a512197097b5108a3407f9f1f0a8d60144867
Andy Rossi <aroccorossi@gmail.com>
2016-03-01 20:51:22 -0500

    added some event stuffs

--- src/script.js
+++ src/script.js
@@ -1,1 +1,40 @@
-console.log("hello");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Hello World!
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('hello')
+);
+
+// Events from a text field
+var TextForm = React.createClass({
+  getInitialState: function() {
+    return {
+      value: '',
+      output: 'Awaiting input...'
+    };
+  },
+  handleChange: function(event) {
+    this.setState( {value: event.target.value} );
+  },
+  handleClick: function(event) {
+    this.setState( {output: this.state.value} );
+  },
+  render: function() {
+    return (
+      <div className="form-container">
+        <input
+          type="text"
+          placeholder="Type something..."
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>Submit</button>
+        <p>{this.state.output}</p>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<TextForm/>, document.getElementById('event'));
